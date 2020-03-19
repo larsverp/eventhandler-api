ff181e2c7679aa8fe89203887319047dc4ce9689
Lars van Erp <larsverp2803@gmail.com>
2020-03-19 16:20:57 +0100

    Created a function that creates a new event when a post request is sent to /api/events :strong:

--- app/Events.php
+++ app/Events.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Events extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'date', 'thumbnail', 'seats', 'postal_code', 'hnum', 'notification'];
 }
--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -16,6 +16,18 @@ class EventsController extends Controller
     }
 
     public function create(){
-        return request()->all();
+        
+        $ValidateAttributes = request()->validate([
+            'title' => 'required|max:191|string|unique:events,title',
+            'description' => 'required|string',
+            'date' => 'required|date|after_or_equal:today',
+            'thumbnail' => 'required|max:191|active_url',
+            'seats' => 'required|integer|min:0',
+            'postal_code' => 'required|postal_code:NL,BE,DE',
+            'hnum' => 'required|max:191|string',
+            'notification' => 'required|boolean'    
+        ]);
+        Events::create($ValidateAttributes);
+        return response($ValidateAttributes, 201);
     }
 }
