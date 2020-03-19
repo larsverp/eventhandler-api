d5421e8045eaaaab04aee9ffba5bde95d817b207
Lars van Erp <larsverp2803@gmail.com>
2020-03-19 13:25:01 +0100

    API is able to show all events and show specific event on ID. Started working on the create function. :simple_smile:

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -7,8 +7,15 @@ use Illuminate\Http\Request;
 
 class EventsController extends Controller
 {
-    public function show(){
-        $event = Events::all();
-        return $event;
+    public function index(){
+        return Events::all();
+    }
+
+    public function show($id){
+        return Events::FindOrFail($id);
+    }
+
+    public function create(){
+        return request()->all();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('events', 'EventsController@show');
+Route::get('events', 'EventsController@index');
+Route::get('events/{id}', 'EventsController@show');
+Route::post('events', 'EventsController@create');
