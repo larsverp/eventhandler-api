4d648b6ec372e7ab999d9ae14e7d2e4f2d73a71f
Lars van Erp <larsverp2803@gmail.com>
2020-03-19 11:59:16 +0100

    Factory for Ruben

---
+++ database/factories/EventsFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Events;
+use Faker\Generator as Faker;
+
+$factory->define(Events::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'description' => $faker->paragraph,
+        'date' => $faker->date,
+        'thumbnail' => $faker->url,
+        'seats' => $faker->randomDigit,
+        'postal_code' => '5673RE',
+        'hnum' => '12A',
+        'notification' => $faker->boolean
+    ];
+});
