d36a2f07c6b1f09a63679df2d6cb9e3e77c9beae
Lars van Erp <larsverp2803@gmail.com>
2020-05-25 13:29:26 +0200

    Changed the way new events look up the location. Hopefully this will increase the speed.

--- app/Events.php
+++ app/Events.php
@@ -7,7 +7,7 @@ use  Illuminate\Support\Str;
 
 class Events extends Model
 {
-    protected $fillable = ['title', 'description', 'host_id', 'begin_date', 'end_date', 'thumbnail', 'seats', 'postal_code', 'hnum', 'notification', 'rockstar'];
+    protected $fillable = ['title', 'description', 'host_id', 'begin_date', 'end_date', 'thumbnail', 'seats', 'postal_code', 'hnum', 'street', 'city', 'notification', 'rockstar'];
     
     protected static function boot()
     {
