4e3a0592ed1e9a58f75bc19587d90a82323b7f2d
Lars van Erp <larsverp2803@gmail.com>
2020-05-26 22:31:56 +0200

    Heroku is complaining about fonts not being loaded.

--- resources/views/pdfs/pdf.blade.php
+++ resources/views/pdfs/pdf.blade.php
@@ -1,15 +1,13 @@
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-
 <p style="float: right;"><img src="images/logo2.png" style="height:250px; width:250px;"></p>
-<h2 style="font-family: 'Open Sans', sans-serif;">Hey {{ $name }},</h2><br>
-<p style="font-family: 'Open Sans', sans-serif;">We're exited to welcome you on our event!</b></p>
-<h4 style="font-family: 'Open Sans', sans-serif;">Event info:</h4>
+<h2>Hey {{ $name }},</h2><br>
+<p>We're exited to welcome you on our event!</b></p>
+<h4>Event info:</h4>
 <table>
-    <tr><th style="text-align:left; padding-right: 8px; font-family: 'Open Sans', sans-serif;">Title:</th><td style="font-family: 'Open Sans', sans-serif;">{{ $event->title }}</td></tr>
-    <tr><th style="text-align:left; padding-right: 8px; font-family: 'Open Sans', sans-serif;">Host:</th><td style="font-family: 'Open Sans', sans-serif;">{{ $host->first_name.' '.$host->last_name }}</td></tr>
-    <tr><th style="text-align:left; padding-right: 8px; font-family: 'Open Sans', sans-serif;">Start:</th><td style="font-family: 'Open Sans', sans-serif;">{{ $event->begin_date }}</td></tr>
-    <tr><th style="text-align:left; padding-right: 8px; font-family: 'Open Sans', sans-serif;">End:</th><td style="font-family: 'Open Sans', sans-serif;">{{ $event->end_date }}</td></tr>
-    <tr><th style="text-align:left; padding-right: 8px; font-family: 'Open Sans', sans-serif;">Address:</th><td style="font-family: 'Open Sans', sans-serif;">{{ $event->street.' '.$event->hnum.', '.$event->postal_code.' '.$event->city }}</td></tr>
+    <tr><th style="text-align:left; padding-right: 8px; ">Title:</th><td >{{ $event->title }}</td></tr>
+    <tr><th style="text-align:left; padding-right: 8px; ">Host:</th><td >{{ $host->first_name.' '.$host->last_name }}</td></tr>
+    <tr><th style="text-align:left; padding-right: 8px; ">Start:</th><td >{{ $event->begin_date }}</td></tr>
+    <tr><th style="text-align:left; padding-right: 8px; ">End:</th><td >{{ $event->end_date }}</td></tr>
+    <tr><th style="text-align:left; padding-right: 8px; ">Address:</th><td >{{ $event->street.' '.$event->hnum.', '.$event->postal_code.' '.$event->city }}</td></tr>
 </table>
 <p style="text-align: center;"><img src="data:image/png;base64, {!! base64_encode($qr) !!} "></p>
-<p style="text-align: center; font-family: 'Open Sans', sans-serif;">Show this ticket at the start of this event.</p>
+<p style="text-align: center; ">Show this ticket at the start of this event.</p>
