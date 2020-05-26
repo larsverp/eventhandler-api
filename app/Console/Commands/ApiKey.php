4c76a73da2654dbdf13e95549c6cccc7f081d076
Lars van Erp <larsverp2803@gmail.com>
2020-05-26 11:24:12 +0200

    Added API key for extra security. Requests to the `api/users/login` endpoit require this api key.

---
+++ app/Console/Commands/ApiKey.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ApiKey extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ApiKey:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create an API key for authenticating the users/login endpoint.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $data = [
+            'grant_type' => 'client_credentials',
+            'client_id' => env('CLIENT2_ID', '2'),
+            'client_secret' => env('CLIENT2_SECRET', 'OY7iuNJtXVssXQ4RjmmRGiwRmTIDxJgheLwRZBHp'),
+        ];
+
+        $request = app('request')->create('/oauth/token', 'POST', $data);
+        $response = app('router')->prepareResponse($request, app()->handle($request));
+
+        $this->info($response);
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
