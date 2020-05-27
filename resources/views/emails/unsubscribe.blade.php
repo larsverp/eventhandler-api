981a984b683ae75d67821fa6d745530d501198c5
Lars van Erp <larsverp2803@gmail.com>
2020-05-27 14:00:16 +0200

    unsubscribe mail

---
+++ resources/views/emails/unsubscribe.blade.php
@@ -0,0 +1,4 @@
+<html>
+<h2 style="text-align: center;"><strong>Hey {{ $name }},</strong></h2><br>
+<h3 style="text-align: center;">You just unsubscribed for <b>"{{ $event->title }}"</b></h3>
+</html>
